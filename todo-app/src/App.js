f46a4edff6f9c6dc3efa18fa742955d2a025397c
idohomri <idohomri@outlook.co.il>
2023-03-25 12:47:37 +0300

    1

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,30 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
+import {v4 as uuid} from "uuid"
+
+const LOCAL_STORAGE_KEY = "todos"
 
 function App() {
-  const [todos, setTodo] = useState([]);
+  const [todos, setTodos] = useState([]);
   const todoNameRef = useRef()
+
+  // load existing todos from local storage
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  // store current session created todos in local storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.completed = !todo.completed
+    setTodos(newTodos)
+  }
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
@@ -12,21 +33,26 @@ function App() {
       return
     }
     // add to todo list
-    setTodo(prevTodos => {
-      return [...prevTodos, {id: 1, name: name, completed: false}]
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuid(), name: name, completed: false}]
     })
 
     // clear textbox
     todoNameRef.current.value = null
   }
 
+  function handleClearCompleted() {
+    const newTodos = todos.filter(todo => !todo.completed)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      <TodoList todos={todos} />
       <input ref={todoNameRef} type="text"></input>
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearCompleted}>Clear Completed</button>
+      <div>{todos.filter(todo => !todo.completed).length} left to do</div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
     </>
   );
 }
