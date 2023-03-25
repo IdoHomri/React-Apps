a7bdce554431a2e87dbde3d81f0cdf0416a7d528
idohomri <idohomri@outlook.co.il>
2023-03-25 12:02:56 +0300

    a

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodo] = useState([]);
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if(name === '') {
+      alert("Todo name can not be empty!");
+      return
+    }
+    // add to todo list
+    setTodo(prevTodos => {
+      return [...prevTodos, {id: 1, name: name, completed: false}]
+    })
+
+    // clear textbox
+    todoNameRef.current.value = null
+  }
+
   return (
-   null
+    <>
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type="text"></input>
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Completed</button>
+      <div>0 left to do</div>
+    </>
   );
 }
 
